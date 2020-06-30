b0f8d3bb0ddca2c1e2393fc6d782c034b7b82243
ngargaud <ngargaud@aldebaran-robotics.com>
2020-06-30 15:22:40 +0200

    add a script to build them all

---
+++ scripts/build_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd zlib && bash build.sh && cd ..
+cd icu && bash build.sh && cd ..
+cd tinyxml && bash build.sh && cd ..
+cd libusb && bash build.sh && cd ..
+cd json-nlohmann && bash build.sh && cd ..
+cd sqlite3 && bash build.sh && cd ..
+cd eigen3 && bash build.sh && cd ..
+cd openssl && bash build.sh && cd ..
+cd boost && bash build.sh && cd ..
