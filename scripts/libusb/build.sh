15b5db2cadff13de13cbbd994268601c8bee1e45
Victor Paleologue <victor.paleologue@softbankrobotics.com>
2021-07-27 10:26:07 +0200

    Fix flags, CMake and build locations
    libqi used to expect using gnu++11 C library, and -fPIC.
    Let us use it for now for compatibility,
    and see if it can be removed later.
    
    The CMake code generated by qitoolchain was not enough for providing
    all the details the usual toolchains gives about Boost.
    Let us recover the usual toolchains' CMake code to ensure compatibility.
    Note that it does not correspond to CMake's usual FindBoost.cmake.
    
    Build locations were modified to reduce the need for `sudo`-ing.
    Installation to the system is done after qitoolchain packaging.

--- scripts/libusb/build.sh
+++ scripts/libusb/build.sh
@@ -3,7 +3,7 @@
 export LIB_VER=${LIB_VER:-"v1.0.22"}
 export NAME="libusb"
 export EXTRACT_DIR="/tmp/${NAME}"
-export INSTALL_DIR="/opt/build/${NAME}"
+export INSTALL_DIR="/tmp/build/${NAME}"
 export GIT_URL="https://github.com/libusb/libusb.git"
 
 function build_and_install()
@@ -12,25 +12,34 @@ function build_and_install()
   if [ $# -eq 0 ]; then
     ./configure
   else
-    ./configure --prefix ${INSTALL_DIR}
+    ./configure --prefix $1
   fi
   make -j4
-  make install
+  if [ $# -eq 0 ]; then
+    # No argument passed: installing in the system.
+    echo "Installing libUSB in the system"
+    sudo make install
+  else
+    # Argument passed: installing in a custom location.
+    echo "Installing libUSB in $1"
+    make install
+  fi
+}
 }
 
 git clone ${GIT_URL} ${EXTRACT_DIR} -b ${LIB_VER}
 cd ${EXTRACT_DIR}
 build_and_install ${INSTALL_DIR}
-echo "${LIB_VER}" >> ${INSTALL_DIR}/VERSION
+sudo echo '${LIB_VER}' | sudo tee -a ${INSTALL_DIR}/VERSION
+
+if [ -x "$(command -v qibuild)" ]; then
+  echo "------------ Building qitoolchain package ----------------"
+  sudo qitoolchain make-package --auto --name ${NAME} --version ${LIB_VER} --target linux64 ${INSTALL_DIR} --output /tmp/workspace/
+else
+  echo "Qibuild is not installed, skipping packaging"
+fi
 
 # install on the system for the next tasks
 build_and_install
 cd ..
-rm -rf ${EXTRACT_DIR}
-
-if [ -x "$(command -v qibuild)" ]; then
-  echo "------------ Building qitoolchain package ----------------"
-  qitoolchain make-package --auto --name ${NAME} --version ${LIB_VER} --target linux64 ${INSTALL_DIR} --output /opt/workspace/
-else
-  echo "Qibuild is not installed, skipping packaging"
-fi
+sudo rm -rf ${EXTRACT_DIR}
